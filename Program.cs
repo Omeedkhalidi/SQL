109b8818dc771b469865a55ea40198027b631e8f
Omeedkhalidi <omeedkhalidi0@gmail.com>
2024-12-10 16:18:39 +0300

    Add files via upload
    Program.cs och repository.cs in för lab 4-uppgiften

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static readonly HttpClient client = new HttpClient();
+
+    static async Task Main()
+    {
+        try
+        {
+            client.DefaultRequestHeaders.Add("User-Agent", "C# console program");
+            string responseBody = await client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            Repository[] repositories = JsonSerializer.Deserialize<Repository[]>(responseBody);
+
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine($"Name: {repo.Name}");
+                Console.WriteLine($"Description: {repo.Description}");
+                Console.WriteLine($"GitHub URL: {repo.HtmlUrl}");
+                Console.WriteLine($"Homepage: {repo.Homepage}");
+                Console.WriteLine($"Watchers: {repo.Watchers}");
+                Console.WriteLine($"Last Pushed: {repo.PushedAt}");
+                Console.WriteLine();
+            }
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("\nException Caught!");
+            Console.WriteLine("Message :{0} ", e.Message);
+        }
+    }
+}
---
+++ Repository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+public class Repository
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("html_url")]
+    public string HtmlUrl { get; set; }
+
+    [JsonPropertyName("homepage")]
+    public string Homepage { get; set; }
+
+    [JsonPropertyName("watchers")]
+    public int Watchers { get; set; }
+
+    [JsonPropertyName("pushed_at")]
+    public DateTime PushedAt { get; set; }
+}
